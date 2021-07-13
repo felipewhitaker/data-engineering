e43928983b7c934f7da3121bf3da74cc8620a3f2
Felipe Whitaker <felipewhitaker@gmail.com>
2021-07-13 15:09:16 -0300

    Finished docker + SQL module

---
+++ docker-module/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql+psycopg2://root:root@localhost/test_db')
+
+df = pd.read_sql_query("SELECT * FROM happiness LIMIT 10", engine)
+
+print(df.head())
