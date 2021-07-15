f26f432407b1c597340cc5a1edbcc2ebc790d49b
Felipe Whitaker <felipewhitaker@gmail.com>
2021-07-15 17:42:42 -0300

    Finished testing

--- tests_module/test_apis.py
+++ tests_module/test_apis.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta
+from os import stat
+
+import requests
 
 import pytest
+from unittest.mock import patch
 from random import sample, random, choice
 
 from poo_module.src.api.currency_api import Currency, DaySummaryAPI, TradeAPI
@@ -12,6 +16,55 @@ def gen_dates():
     while True:
         yield start_date + random() * (end_date - start_date)
 
+def mock_requests_get(*args, **kwargs):
+    class MockResponse(requests.Response):
+        def __init__(self, json_data, status_code):
+            super().__init__()
+            self.status_code = status_code
+            self.json_data = json_data
+
+        def json(self):
+            return self.json_data
+
+        def raise_for_status(self) -> None:
+            if self.status_code == 200:
+                return None
+            raise requests.RequestException
+
+    if not args[0].endswith('invalid_endpoint'):
+        return MockResponse(json_data = {'foo': 'bar'}, status_code = 200)
+    else:
+        return MockResponse(json_data = None, status_code = 500)
+
+@pytest.fixture
+@patch('poo_module.src.api.currency_api.Currency.__abstractmethods__', set())
+def fixture_currency():
+    return Currency(currency = 'BTC')
+
+# @patch('poo_module.src.api.currency_api.Currency.__abstractmethods__', set())
+class TestCurrency:
+    
+    @patch('requests.get', side_effect = mock_requests_get)
+    @patch('poo_module.src.api.currency_api.Currency._get_endpoint', return_value = 'valid_endpoint')
+    def test_get_data_requests_is_called(self, mock_get_endpoint, mock_requests, fixture_currency):
+        actual = fixture_currency.get_data()
+        mock_requests.assert_called_once_with(Currency.BASE_URL + 'valid_endpoint')
+
+    @patch('requests.get', side_effect = mock_requests_get)
+    @patch('poo_module.src.api.currency_api.Currency._get_endpoint', return_value = 'valid_endpoint')
+    def test_get_data_with_valid_endpoint(self, mock_get_endpoint, mock_requests, fixture_currency):
+        actual = fixture_currency.get_data()
+        expected = {'foo': 'bar'}
+        print('Actual: ', actual)
+        assert actual == expected
+
+    @patch('requests.get', side_effect = mock_requests_get)
+    @patch('poo_module.src.api.currency_api.Currency._get_endpoint', return_value = 'invalid_endpoint')
+    def test_get_data_with_invalid_endpoint(self, mock_get_endpoint, mock_requests, fixture_currency):
+        with pytest.raises(requests.RequestException):
+            actual = fixture_currency.get_data()
+            print('invalid: ', actual)
+
 
 class TestDaySummaryAPI:
     @pytest.mark.parametrize(
@@ -22,7 +75,9 @@ class TestDaySummaryAPI:
                 date,
                 f"{currency}/day-summary/{date.year}/{date.month}/{date.day}/",
             ]
-            for currency, date in zip(sample(Currency.CURRENCIES, 5), gen_dates())
+            for currency, date in zip(['BTC'], gen_dates())
+            # for currency, date in zip(sample(DaySummaryAPI.CURRENCIES, 5), gen_dates())
+            # TODO fix 5 currencies instead of using sample as its O(n)
         ],
     )
     def test_get_endpoint(self, currency, date, expected):
@@ -40,7 +95,7 @@ class TestTradeAPI:
 
     @pytest.mark.parametrize("date, expected", [])
     def test_get_unix_epoch(self, date, expected):
-        api = TradeAPI(choice(TradeAPI.CURRENCIES))
+        api = TradeAPI(currency='BTC')
         assert api._get_unix_epoch(date) == expected
 
     @pytest.mark.parametrize("", [])
--- tests_module/test_ingestor.py
+++ tests_module/test_ingestor.py
@@ -2,36 +2,35 @@ from datetime import datetime
 from poo_module.src.ingestor.ingestor import DataIngestor
 from poo_module.src.writer.writer import DataWriter
 
+import pytest
 from unittest.mock import mock_open, patch
+
+
+@pytest.fixture
+@patch("poo_module.src.ingestor.ingestor.DataIngestor.__abstractmethods__", set())
+def data_ingestor_fixture():
+    return DataIngestor(
+        writer=DataWriter,
+        currencies=["BTC", "ETH"],
+        default_start_date=datetime(2021, 6, 21),
+    )
 
 
 @patch("poo_module.src.ingestor.ingestor.DataIngestor.__abstractmethods__", set())
 class TestIngestors:
-    def test_checkpoint_filename(self):
-        actual = DataIngestor(
-            writer=DataWriter,
-            currencies=["BTC", "ETH"],
-            default_start_date=datetime(2021, 6, 21),
-        )._checkpoint_filename
+    def test_checkpoint_filename(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._checkpoint_filename
         expected = "DataIngestor.checkpoint"
         assert actual == expected
 
-    def test_load_checkpoint_no_checkpoint(self):
-        actual = DataIngestor(
-            writer=DataWriter,
-            currencies=["BTC", "ETH"],
-            default_start_date=datetime(2021, 6, 21),
-        )._load_checkpoint()
+    def test_load_checkpoint_no_checkpoint(self, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
         expected = None
         assert actual == expected
 
     @patch("builtins.open", new_callable=mock_open, read_data="2021-6-25")
-    def test_load_checkpoint_existing_checkpoint(self, mock):
-        actual = DataIngestor(
-            writer=DataWriter,
-            currencies=["BTC", "ETH"],
-            default_start_date=datetime(2021, 6, 21),
-        )._load_checkpoint()
+    def test_load_checkpoint_existing_checkpoint(self, mock, data_ingestor_fixture):
+        actual = data_ingestor_fixture._load_checkpoint()
         expected = datetime(2021, 6, 25)
         assert actual == expected
 
@@ -39,12 +38,20 @@ class TestIngestors:
         "poo_module.src.ingestor.ingestor.DataIngestor._write_checkpoint",
         return_value=None,
     )
-    def test_update_checkpoint_checkpoint_updated(self, mock):
-        di = DataIngestor(
-            writer=DataWriter,
-            currencies=["BTC", "ETH"],
-            default_start_date=datetime(2021, 6, 21),
-        )
+    def test_update_checkpoint_checkpoint_updated(self, mock, data_ingestor_fixture):
         expected = datetime(2019, 1, 1)
+        di = data_ingestor_fixture
         di.checkpoint = expected
         assert di.checkpoint == expected
+        mock.assert_called_once()
+
+    @patch("builtins.open", new_callable=mock_open, read_data="2021-6-25")
+    @patch(
+        "poo_module.src.ingestor.ingestor.DataIngestor._checkpoint_filename",
+        return_value="foobar.checkpoint",
+    )
+    def test_write_checkpoint(
+        self, mock_checkpoint_file_name, mock_open, data_ingestor_fixture
+    ):
+        data_ingestor_fixture._write_checkpoint()
+        mock_open.assert_called_with(mock_checkpoint_file_name, "w")
