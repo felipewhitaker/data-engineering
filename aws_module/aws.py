5220a7d99a2f5ea6e2223c5e6225c851218fdb5d
Felipe Whitaker <felipewhitaker@gmail.com>
2021-07-20 20:47:07 -0300

    Created and Deleted s3 instance

---
+++ aws_module/aws.py
@@ -0,0 +1,35 @@
+from typing import get_args
+import boto3
+
+import time
+
+from botocore import exceptions
+from botocore.exceptions import ClientError
+
+import logging
+import logging
+logging.getLogger()
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.ERROR)
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv('./.env')
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = getenv('AWS_ID'),
+    aws_secret_access_key = getenv('AWS_KEY')
+)
+
+bucket_name = 'thor-s3-bucket'
+try:
+    s3_client.create_bucket(Bucket = bucket_name)
+    logging.info(f'Created {bucket_name} with success')
+except ClientError:
+    logging.error(f'Could not create {bucket_name}')
+    quit()
+
+time.sleep(10)
+s3_client.delete_bucket(Bucket = bucket_name)
